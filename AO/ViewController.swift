eb552a940ee08e132aca480089d54fe41d16a8b7
Tahya Mumtahi <88910928+tahya765@users.noreply.github.com>
2022-08-10 14:45:45 -0400

    Add files via upload

---
+++ AO/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AO
+//
+//  Created by Scholar on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        
+        if let newTitle = textField.text{
+            appTitle.text = newTitle
+        }
+    }
+    
+}
+
